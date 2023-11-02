37578c37d6800a128b1271f7b75194673077a72c
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-11-02 20:33:37 +0300

    completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete, onUpdate  }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,20 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleUpdate(event) {
+    const newCorrectIndex = parseInt(event.target.value);
+    onUpdate(id, newCorrectIndex);
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleUpdate}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={onDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,18 @@
 import React from "react";
+import QuestionItem from "./QuestionItem.js"
 
-function QuestionList() {
+function QuestionList({ questions, onDelete, onUpdate }) {
+
+  const quizQuestion = questions.map((question) => {
+    return <QuestionItem key={question.id} question={question} 
+    onDelete={() => onDelete(question.id)} 
+    onUpdate={(newCorrectIndex) => onUpdate(question.id, newCorrectIndex)}/> 
+  })
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{quizQuestion}</ul>
     </section>
   );
 }
